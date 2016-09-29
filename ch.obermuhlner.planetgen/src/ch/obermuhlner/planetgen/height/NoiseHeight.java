efbdb97a81c06f2e1bee409847966f11c3b30923
Eric Obermühlner <eobermuhlner@gmail.com>
2016-09-29 18:17:32 +0200

    use Vector3.ofPolar()

--- ch.obermuhlner.planetgen/src/ch/obermuhlner/planetgen/height/NoiseHeight.java
+++ ch.obermuhlner.planetgen/src/ch/obermuhlner/planetgen/height/NoiseHeight.java
@@ -1,5 +1,7 @@
 package ch.obermuhlner.planetgen.height;
 
+import ch.obermuhlner.planetgen.math.Vector3;
+
 public class NoiseHeight implements Height {
 
 	private final FractalNoise fractalNoise;
@@ -14,17 +16,9 @@ public class NoiseHeight implements Height {
 
     @Override
     public double height(double latitude, double longitude, double accuracy) {
-    	double radius = 1.0;
-    	double sinLongitude = Math.sin(longitude);
-    	double cosLongitude = Math.cos(longitude);
-    	double sinLatitude = Math.sin(latitude);
-    	double cosLatitude = Math.cos(latitude);
+    	Vector3 cartesian = Vector3.ofPolar(latitude, longitude, 1.0);
     	
-    	double x = radius * cosLongitude * sinLatitude;
-    	double y = radius * sinLongitude * sinLatitude;
-    	double z = radius * cosLatitude;
-    	
-    	double noise = fractalNoise.getNoise(x, y, z) * 0.5 + 0.5;
+    	double noise = fractalNoise.getNoise(cartesian.x, cartesian.y, cartesian.z) * 0.5 + 0.5;
 		double height = (maxHeight - minHeight) * noise + minHeight;
 		return height;
     }
