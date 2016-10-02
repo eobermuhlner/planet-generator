a12ea19bd918d3ec1753113b8d57111076ed038e
Eric Obermühlner <eobermuhlner@gmail.com>
2016-10-02 23:07:25 +0200

    cleanup Tuple2

--- ch.obermuhlner.planetgen/src/ch/obermuhlner/util/Tuple2.java
+++ ch.obermuhlner.planetgen/src/ch/obermuhlner/util/Tuple2.java
@@ -5,6 +5,7 @@
 package ch.obermuhlner.util;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * A tuple of two values.
@@ -18,12 +19,10 @@ import java.io.Serializable;
  */
 public class Tuple2<T1, T2> implements Serializable {
 
-	private static final long serialVersionUID = 1087567343343376526L;
+	private static final long serialVersionUID = 1L;
 
 	private final T1 value1;
 	private final T2 value2;
-
-	private transient int hashCode;
 
 	/**
 	 * Constructs a {@link Tuple2}.
@@ -32,7 +31,7 @@ public class Tuple2<T1, T2> implements Serializable {
 	 * @param theValue2 the second value
 	 * @see Tuples#tuple(Object, Object)
 	 */
-	public Tuple2(T1 theValue1, T2 theValue2) {
+	private Tuple2(T1 theValue1, T2 theValue2) {
 		value1 = theValue1;
 		value2 = theValue2;
 	}
@@ -57,31 +56,29 @@ public class Tuple2<T1, T2> implements Serializable {
 
 	@Override
 	public int hashCode() {
-		if (hashCode == 0) {
-			int hash = 7;
-			hash = 31 * hash + (value1 == null ? 23 : value1.hashCode());
-			hash = 31 * hash + (value2 == null ? 23 : value2.hashCode());
-			hashCode = hash;
-		}
-		return hashCode;
+		return Objects.hash(value1, value2);
 	}
 
 	@Override
-	public boolean equals(Object theObject) {
-		if (theObject == this) {
+	public boolean equals(Object object) {
+		if (object == this) {
 			return true;
 		}
-		if (!(theObject instanceof Tuple2)) {
+		if (!(object instanceof Tuple2)) {
 			return false;
 		}
 
-		Tuple2<?, ?> theOther = (Tuple2<?, ?>) theObject;
-		return (value1 == null ? theOther.value1 == null : value1.equals(theOther.value1)) && //
-				(value2 == null ? theOther.value2 == null : value2.equals(theOther.value2));
+		Tuple2<?, ?> other = (Tuple2<?, ?>) object;
+		return (value1 == null ? other.value1 == null : value1.equals(other.value1)) && //
+				(value2 == null ? other.value2 == null : value2.equals(other.value2));
 	}
 
 	@Override
 	public String toString() {
 		return "(" + value1 + "," + value2 + ")";
+	}
+	
+	public static <T1, T2> Tuple2<T1, T2> of(T1 value1, T2 value2) {
+		return new Tuple2<T1, T2>(value1, value2);
 	}
 }
