c3c80ccb868dba6bee8ba6b5c4a5602b8735b89a
Eric Obermühlner <eobermuhlner@gmail.com>
2016-10-04 19:50:47 +0200

    more fine tuning plant layer

--- ch.obermuhlner.planetgen/src/ch/obermuhlner/planetgen/generator/PlanetGenerator.java
+++ ch.obermuhlner.planetgen/src/ch/obermuhlner/planetgen/generator/PlanetGenerator.java
@@ -105,11 +105,11 @@ public class PlanetGenerator {
 		planet.layers.put("Plants", new PlantLayer(
 				Arrays.asList(
 						PlantData.of("Tundra", 0.1, 0.1, 0.1, Units.celsiusToKelvin(-5), 10, 10, 0, Color.YELLOWGREEN),
-						PlantData.of("Grass", 0.2, 0.1, 0.2, Units.celsiusToKelvin(15), 20, 20, 0, Color.LAWNGREEN),
+						PlantData.of("Grass", 0.2, 0.3, 0.2, Units.celsiusToKelvin(15), 20, 20, 0, Color.LAWNGREEN),
 						PlantData.of("Savanna", 0.2, 0.2, 0.1, Units.celsiusToKelvin(30), 20, 20, 0, Color.YELLOW),
-						PlantData.of("Arctic rainforest", 0.5, 0.5, 0.5, Units.celsiusToKelvin(5), 5, 5, -1, Color.SEAGREEN),
-						PlantData.of("Temperate forest", 0.2, 0.4, 0.2, Units.celsiusToKelvin(5), 20, 20, 0, Color.DARKGREEN),
-						PlantData.of("Temperate rainforest", 1.0, 1.0, 1.0, Units.celsiusToKelvin(10), 20, 20, -1, Color.GREEN),
+						PlantData.of("Arctic rainforest", 0.5, 0.5, 0.5, Units.celsiusToKelvin(5), 10, 5, -1, Color.SEAGREEN),
+						PlantData.of("Temperate forest", 0.2, 0.4, 0.2, Units.celsiusToKelvin(10), 10, 10, 0, Color.DARKGREEN),
+						PlantData.of("Temperate rainforest", 1.0, 1.0, 1.0, Units.celsiusToKelvin(15), 10, 10, -1, Color.GREEN),
 						PlantData.of("Sub-tropical rainforest", 1.0, 2.0, 2.0, Units.celsiusToKelvin(20), 15, 15, -1, Color.DARKOLIVEGREEN),
 						PlantData.of("Tropical rainforest", 1.0, 2.0, 2.0, Units.celsiusToKelvin(30), 20, 20, -1, Color.FORESTGREEN))));
 		planet.layers.put("Snow", new SnowLayer(
