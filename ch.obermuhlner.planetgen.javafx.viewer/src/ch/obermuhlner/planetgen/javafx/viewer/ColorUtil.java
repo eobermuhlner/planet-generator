a380d1cb558038bf48b3b0ae689d3f9c0a789414
Eric Obermühlner <eobermuhlner@gmail.com>
2016-10-05 07:35:06 +0200

    toJavafxColor() converts null to TRANSPARENT

--- ch.obermuhlner.planetgen.javafx.viewer/src/ch/obermuhlner/planetgen/javafx/viewer/ColorUtil.java
+++ ch.obermuhlner.planetgen.javafx.viewer/src/ch/obermuhlner/planetgen/javafx/viewer/ColorUtil.java
@@ -3,6 +3,9 @@ package ch.obermuhlner.planetgen.javafx.viewer;
 public class ColorUtil {
 
 	public static javafx.scene.paint.Color toJavafxColor(ch.obermuhlner.planetgen.math.Color color) {
+		if (color == null) {
+			return javafx.scene.paint.Color.TRANSPARENT;
+		}
 		return new javafx.scene.paint.Color(color.getRed(), color.getGreen(), color.getBlue(), 1);
 	}
 }
