02ad4ff6b096a885cfb456ead2e507ec21b8a2d2
Eric Obermühlner <eobermuhlner@gmail.com>
2016-10-25 19:49:29 +0200

    need to discover accurate random crater size distribution

--- ch.obermuhlner.planetgen/src/ch/obermuhlner/planetgen/planet/layer/CraterLayer.java
+++ ch.obermuhlner.planetgen/src/ch/obermuhlner/planetgen/planet/layer/CraterLayer.java
@@ -95,7 +95,7 @@ public class CraterLayer implements Layer {
 				return;
 			}
 			
-			double randomSize = random.nextDouble(0.1, 0.49);
+			double randomSize = random.nextDouble(0.1, 0.7);
 			gridSize *= randomSize;
 
 			Vector2 randomDisplacement = Vector2.of(
