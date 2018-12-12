a041e15a5c8a59b0ff7a5f188254f80e6fc712f7
Eric Obermühlner <eobermuhlner@gmail.com>
2018-12-12 21:28:40 +0100

    correct mix of specular colors in ground layer

--- ch.obermuhlner.planetgen/src/main/java/ch/obermuhlner/planetgen/planet/layer/GroundLayer.java
+++ ch.obermuhlner.planetgen/src/main/java/ch/obermuhlner/planetgen/planet/layer/GroundLayer.java
@@ -46,15 +46,17 @@ public class GroundLayer implements Layer {
 		} else {
 			double relativeHeight = planetPoint.height / planet.planetData.maxHeight;
 			if (relativeHeight < 0.5) {
-				planetPoint.groundColor = lowGroundColor.interpolate(midGroundColor, relativeHeight * 2.0);
-				planetPoint.specularColor = GROUND1_SPECULAR_COLOR.interpolate(GROUND2_SPECULAR_COLOR, relativeHeight);
+				double mix = relativeHeight * 2.0;
+				planetPoint.groundColor = lowGroundColor.interpolate(midGroundColor, mix);
+				planetPoint.specularColor = GROUND1_SPECULAR_COLOR.interpolate(GROUND2_SPECULAR_COLOR, mix);
 			} else {
 				double layerHeight = layerFunction.vector2Value(Vector2.of(1.0, relativeHeight), 0.000001);
 				planetPoint.debug = layerHeight;
 				Color layeredColor = highGroundColor1.interpolate(highGroundColor2, layerHeight);
 				planetPoint.groundColor = layeredColor;
-				planetPoint.groundColor = midGroundColor.interpolate(layeredColor, (relativeHeight - 0.5) * 2.0);
-				planetPoint.specularColor = GROUND1_SPECULAR_COLOR.interpolate(GROUND2_SPECULAR_COLOR, relativeHeight);
+				double mix = (relativeHeight - 0.5) * 2.0;
+				planetPoint.groundColor = midGroundColor.interpolate(layeredColor, mix);
+				planetPoint.specularColor = GROUND1_SPECULAR_COLOR.interpolate(GROUND2_SPECULAR_COLOR, mix);
 			}
 		}
 		planetPoint.color = planetPoint.groundColor;
